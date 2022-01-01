4355e15a9b1338f159fd5b69e50a9089872b9239
fiatjaf <fiatjaf@gmail.com>
2022-01-01 11:19:14 -0300

    shrinking URLs when necessary.

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -18,6 +18,39 @@ md.linkify
   .tlds(['onion', 'eth'], true)
   .add('bitcoin:', null)
   .add('lightning:', null)
+  .add('http:', {
+    validate(text, pos, self) {
+      // copied from linkify defaultSchemas
+      var tail = text.slice(pos)
+      if (!self.re.http) {
+        self.re.http = new RegExp(
+          '^\\/\\/' +
+            self.re.src_auth +
+            self.re.src_host_port_strict +
+            self.re.src_path,
+          'i'
+        )
+      }
+      if (self.re.http.test(tail)) {
+        return tail.match(self.re.http)[0].length
+      }
+      return 0
+    },
+    normalize(match, self) {
+      if (self.__text_cache__.length < 150 || match.text < 23) {
+        return
+      }
+
+      let url = new URL(match.url)
+      let text = url.host
+      if ((url.pathname + url.search + url.hash).length > 10) {
+        let suffix = match.text.slice(-7)
+        if (suffix[0] === '/') suffix = suffix.slice(1)
+        text += `/…/${suffix}`
+      }
+      match.text = text
+    }
+  })
   .set({fuzzyEmail: false})
 
 export default {

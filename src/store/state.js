68ff01c64b76556672e0828cb79cd14aecde41a8
fiatjaf <fiatjaf@gmail.com>
2022-01-16 21:36:24 -0300

    initial set of relays made somewhat random.

--- src/store/state.js
+++ src/store/state.js
@@ -1,17 +1,29 @@
 import {LocalStorage} from 'quasar'
 
 export default function () {
+  const relays = {
+    'wss://nostr-pub.wellorder.net': {read: true, write: true},
+    'wss://rsslay.fiatjaf.com': {read: true, write: false}
+  }
+  const optional = [
+    ['wss://nostr.rocks', {read: true, write: true}],
+    ['wss://relayer.fiatjaf.com', {read: true, write: true}],
+    ['wss://nostrrr.bublina.eu.org', {read: true, write: true}],
+    ['wss://nostr-relay.wlvs.space', {read: true, write: true}],
+    ['wss://nostr.bitcoiner.social', {read: true, write: true}],
+    ['wss://nostr-relay.freeberty.net', {read: true, write: true}]
+  ]
+  for (let i = 0; i < 3; i++) {
+    let pick = parseInt(Math.random() * optional.length)
+    let [url, prefs] = optional[pick]
+    relays[url] = prefs
+    optional.splice(pick, 1)
+  }
+
   return {
     keys: LocalStorage.getItem('keys') || {}, // {priv, pub }
 
-    relays: {
-      // 'wss://nostr.rocks': {read: true, write: true},
-      'wss://relayer.fiatjaf.com': {read: true, write: true},
-      // 'wss://nostrrr.bublina.eu.org': {read: true, write: true},
-      'wss://nostr-pub.wellorder.net': {read: true, write: true}
-      // 'wss://nostr-relay.freeberty.net': {read: true, write: true},
-      // 'wss://freedom-relay.herokuapp.com/ws': {read: true, write: true}
-    }, // { [url]: {} }
+    relays, // { [url]: {} }
     following: [], // [ pubkeys... ]
 
     profilesCache: {}, // { [pubkey]: {name, about, picture, ...} }

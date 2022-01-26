62fb9859204f1c8ddf3cfce1bd398f2f291eda14
fiatjaf <fiatjaf@gmail.com>
2022-01-26 17:47:32 -0300

    show messages tab on window.nostr.nip04.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -48,7 +48,7 @@
             </q-item>
 
             <q-item
-              v-if="!!$store.state.keys.priv"
+              v-if="$store.getter.canEncryptDecrypt"
               v-ripple
               clickable
               to="/messages"
@@ -167,7 +167,7 @@
         </q-badge>
       </q-route-tab>
       <q-route-tab
-        v-if="!!$store.state.keys.priv"
+        v-if="$store.getters.canEncryptDecrypt"
         icon="email"
         to="/messages"
         active-class=""

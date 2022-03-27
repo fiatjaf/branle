52e82c11be289e2b1439f8567d37219fb880fc06
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-03-27 20:12:23 +0000

    fix: check for name with correct var

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="px-4 pt-6">
-    <div v-if="$store.getters.hasName(pubkey)" class="text-xl">Hello <Name :pubkey="$store.state.keys.pub" /></div>
+    <div v-if="$store.getters.hasName($store.state.keys.pub)" class="text-xl">Hello <Name :pubkey="$store.state.keys.pub" /></div>
     <div v-else class="text-xl">Home</div>
 
     <Publish />
